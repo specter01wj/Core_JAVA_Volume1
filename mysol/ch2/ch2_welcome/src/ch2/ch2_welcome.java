01e7b38b0a246c02d9bd075a5db078cf69ab5e0a
specter01wj <specter01wj@gmail.com>
2018-09-11 01:06:22 -0400

    09/10/2018 v1

---
+++ mysol/ch2/ch2_welcome/src/ch2/ch2_welcome.java
@@ -0,0 +1,16 @@
+package ch2;
+
+public class ch2_welcome {
+
+	public static void main(String[] args){
+		
+		String greeting = "Welcome to Core Java!";
+	    System.out.println(greeting);
+	    for (int i = 0; i < greeting.length(); i++){
+	    	System.out.print("=");
+	    }
+	    System.out.println();
+	      
+	}
+	
+}
