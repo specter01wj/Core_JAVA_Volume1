a4d29517d532ac51e622d1040b1c9afb14439097
specter01wj <specter01wj@gmail.com>
2018-09-19 01:23:57 -0400

    09/18/2018 v1

--- mysol/ch4/ch4_1_calendarTest/src/ch4_1_calendarTest/ch4_1_calendarTest.java
+++ mysol/ch4/ch4_1_calendarTest/src/ch4_1_calendarTest/ch4_1_calendarTest.java
@@ -1,8 +1,39 @@
 package ch4_1_calendarTest;
 
+import java.time.*;
+
 public class ch4_1_calendarTest {
 
 	public static void main(String[] args) {
+		
+		LocalDate date = LocalDate.now();
+		int month = date.getMonthValue(), today = date.getDayOfMonth();
+		
+		date = date.minusDays(today - 1);
+		DayOfWeek weekday = date.getDayOfWeek();
+		int value = weekday.getValue();
+		
+		System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+		for(int i = 1; i < value; i++){
+			System.out.print("   ");
+		}
+		
+		while(date.getMonthValue() == month) {
+			System.out.printf("%3d", date.getDayOfMonth());
+			if(date.getDayOfMonth() == today) {
+				System.out.print("*");
+			} else {
+				System.out.print(" ");
+			}
+			date = date.plusDays(1);
+			if(date.getDayOfWeek().getValue() == 1) {
+				System.out.println();
+			}
+		}
+		if(date.getDayOfWeek().getValue() != 1) {
+			System.out.println();
+		}
+		
 		
 	}
 	
