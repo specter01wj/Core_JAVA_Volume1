ab8062411ffca6ac76abcc2e3e6c2b11856b49d8
specter01wj <specter01wj@gmail.com>
2018-09-27 15:34:18 -0400

    09/27/2018 v1

--- mysol/ch5/ch5_11_arrayListTest/src/ch5_11_arrayListTest/ArrayListTest.java
+++ mysol/ch5/ch5_11_arrayListTest/src/ch5_11_arrayListTest/ArrayListTest.java
@@ -7,10 +7,10 @@ public class ArrayListTest {
 	public static void main(String[] args) {
 		
 		ArrayList<Employee> staff = new ArrayList<>();
-		Employee[] staff = new Employee[3];
-		staff[0] = new Employee("James Wang", 75000, 1987, 12, 15);
-		staff[1] = new Employee("Harry Hacker", 45000, 1989, 10, 11);
-		staff[2] = new Employee("Tony Yuan", 105000, 1985, 06, 22);
+		
+		staff.add(new Employee("James Wang", 75000, 1987, 12, 15));
+		staff.add(new Employee("Harry Hacker", 45000, 1989, 10, 11));
+		staff.add(new Employee("Tony Yuan", 105000, 1985, 06, 22));
 		
 		for(Employee e : staff) {
 			e.raiseSalary(5);
