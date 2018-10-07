5e7aa26c6456a013b1ee4a62b0db113ffed3522f
specter01wj <specter01wj@gmail.com>
2018-10-06 21:45:12 -0400

    10/06/2018 v1

---
+++ mysol/ch6/ch6_7_innerClassTest/src/ch6_7_innerClassTest/InnerClassTest.java
@@ -0,0 +1,44 @@
+package ch6_7_innerClassTest;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+public class InnerClassTest {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
+
+class TalkingClock {
+	
+	private int interval;
+	private boolean beep;
+	
+	public TalkingClock(int interval, boolean beep) {
+		this.interval = interval;
+		this.beep = beep;
+	}
+	
+	public void start() {
+		ActionListener listener = new TimePrinter();
+		Timer t = new Timer(interval, listener);
+		t.start();
+	}
+	
+	public class TimePrinter implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("At the tone, the time is: " + new Date());
+			if(beep) {
+				Toolkit.getDefaultToolkit().beep();
+			}
+		}
+	}
+	
+}
