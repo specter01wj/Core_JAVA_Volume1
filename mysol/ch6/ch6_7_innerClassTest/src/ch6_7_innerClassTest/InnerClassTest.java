a3554b7b4d515077bb9fde4d3929bbfd655725e2
specter01wj <specter01wj@gmail.com>
2018-10-06 21:48:14 -0400

    10/06/2018 v1

--- mysol/ch6/ch6_7_innerClassTest/src/ch6_7_innerClassTest/InnerClassTest.java
+++ mysol/ch6/ch6_7_innerClassTest/src/ch6_7_innerClassTest/InnerClassTest.java
@@ -10,7 +10,11 @@ public class InnerClassTest {
 	
 	public static void main(String[] args) {
 		
+		TalkingClock clock = new TalkingClock(1000, true);
+		clock.start();
 		
+		JOptionPane.showMessageDialog(null,  "Quit Mess!");
+		System.exit(0);
 		
 	}
 
