d7138953e0689df629ec27ac8028e4ecbef9b1cb
specter01wj <specter01wj@gmail.com>
2018-10-16 14:37:40 -0400

    10/16/2018 v1

--- mysol/ch8/ch8_1_pairTest1/src/ch8_1_pairTest1/Pair.java
+++ mysol/ch8/ch8_1_pairTest1/src/ch8_1_pairTest1/Pair.java
@@ -6,11 +6,15 @@ public class Pair<T> {
 	   private T second;
 
 	   public Pair() { first = null; second = null; }
-	   public Pair(T first, T second) { this.first = first;  this.second = second; }
+	   
+	   public Pair(T first, T second) { 
+		   this.first = first;  
+		   this.second = second; 
+	   }
 
 	   public T getFirst() { return first; }
 	   public T getSecond() { return second; }
 
-	   public void setFirst(T newValue) { first = newValue; }
-	   public void setSecond(T newValue) { second = newValue; }
+	   public void setFirst(T newValue) { this.first = newValue; }
+	   public void setSecond(T newValue) { this.second = newValue; }
 }
